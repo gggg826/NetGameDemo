1c32846dd8159494a033645fb783e5637c174ed1
gggg826 <liushida87@126.com>
2017-06-05 23:42:23 +0800

    Add SocketServer.cs and main method

---
+++ Server/Server/SocketSever.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading;
+
+//1.socket
+//2.socketasynceventargs
+//3.Semaphore 阻塞进程
+//4.去借鉴抢滩的Protobuf管理
+
+namespace Server
+{
+    public class SocketSever
+    {
+        private Semaphore m_Semaphore;
+
+        public SocketSever()
+        {
+
+        }
+
+        public void ClientStart()
+        {
+
+        }
+
+        public void ClientClose()
+        {
+
+        }
+
+        private void StartAccept(SocketAsyncEventArgs e)
+        {
+
+        }
+
+        private void ProcessAccept(SocketAsyncEventArgs e)
+        {
+
+        }
+
+        private void StartReceive(SocketAsyncEventArgs e)
+        {
+
+        }
+
+        private void ProcessReceive(SocketAsyncEventArgs e)
+        {
+
+        }
+
+        private void ProcessSend(SocketAsyncEventArgs e)
+        {
+
+        }
+
+        private void OnAcceptCompleted(SocketAsyncEventArgs e)
+        {
+
+        }
+
+        private void OnIOCompleted(SocketAsyncEventArgs e)
+        {
+
+        }
+    }
+}
