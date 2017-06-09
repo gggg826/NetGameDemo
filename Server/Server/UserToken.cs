cf28c8c99f6e49391ec53f3ad552af2fdc995338
gggg826 <liushida87@126.com>
2017-06-09 10:44:59 +0800

    Delete the folder of NetSystem.

---
+++ Server/Server/UserToken.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Server
+{
+	public class UserToken
+	{
+		public Socket SocketConnected;
+		public UserToken()
+		{
+
+		}
+	}
+}
